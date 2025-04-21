8e55495bc3eb30b931342b71a633be238c286091
Nqobile <nqobilemasombuka@iclud.com>
2025-04-21 22:54:47 +0200

    Serve frontend in production

--- server/index.js
+++ server/index.js
@@ -35,6 +35,21 @@ app.use(cookieParser());
 //app.use(morgan("dev"));
 app.use("/api", routes);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(routeNotFound);
 app.use(errorHandler);
 
